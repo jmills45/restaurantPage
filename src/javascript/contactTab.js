8c3fd5bef3f79e67a84dabdc86346756e4cdb741
Joshua Mills <jmillsman33@gmail.com>
2023-10-22 23:58:19 -0500

    Added contact tab and adjusted css for desktop
    Added a basic contact us tab that will later include
    google map intergration.
    
    Adjusted css with media query for better display
    on desktop / non-mobile devices.

--- src/javascript/contactTab.js
+++ src/javascript/contactTab.js
@@ -1,7 +1,21 @@
+// Import images
+import mapImage from '../images/map.png';
+
 const createContactTab = () => {
     const contactContent = document.createElement('div');
+    contactContent.classList.add('contact');
+
+    const map = createGoogleMap();
+    contactContent.appendChild(map);
+
 
     return contactContent;
 }
 
 export { createContactTab };
+
+function createGoogleMap(){
+    const mapElement = document.createElement('img');
+    mapElement.src = mapImage;
+    return mapElement;
+}
